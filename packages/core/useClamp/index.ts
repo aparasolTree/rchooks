29544e06090e578e1ff7d1411ba6463f0f2db937
aparasolTree <LikeParasolTree@163.com>
2022-06-13 15:14:00 +0800

    fix: useClamp hook

--- packages/core/useClamp/index.ts
+++ packages/core/useClamp/index.ts
@@ -1,11 +1,16 @@
 import { clamp } from '@r-hooks/shared';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
-export function useClamp(min: number, max: number) {
-    return useCallback(
-        (value: number) => {
-            return clamp(value, min, max);
+export function useClamp(initialValue: number, min: number, max: number) {
+    const [value, _setValue] = useState(initialValue);
+    const setValue = useCallback(
+        (value: number | ((prevValue: number) => number)) => {
+            return _setValue((_value) => {
+                return clamp(typeof value === 'function' ? value(_value) : value, min, max);
+            });
         },
         [min, max]
     );
+
+    return [value, setValue] as const;
 }
