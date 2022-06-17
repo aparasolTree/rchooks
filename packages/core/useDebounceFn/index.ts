e4156d795289bd09aad6e80e12635cb255c6ccca
aparasolTree <LikeParasolTree@163.com>
2022-06-17 17:43:23 +0800

    perf(debounce, useDeounceFn): add immediateCallback option

--- packages/core/useDebounceFn/index.ts
+++ packages/core/useDebounceFn/index.ts
@@ -4,11 +4,11 @@ import { useLatest } from '../useLatest';
 import { useUnmount } from '../useUnmount';
 
 export function useDebounceFn<T extends Fn>(fn: T, ms: number, options: DebounceOptions = {}) {
-    const { immediate = true } = options;
+    const { immediate = true, immediateCallback = true } = options;
     const fnRef = useLatest(fn);
     const [debounceWrap, clear] = useMemo(
-        () => debounce<T>(fnRef.current, ms, { immediate }),
-        [ms, immediate, fnRef]
+        () => debounce<T>(fnRef.current, ms, { immediate, immediateCallback }),
+        [ms, immediate, fnRef, immediateCallback]
     );
 
     useUnmount(() => clear());
--- packages/shared/debounce/index.ts
+++ packages/shared/debounce/index.ts
@@ -2,10 +2,11 @@ import { Fn } from '../utils';
 
 export interface DebounceOptions {
     immediate?: boolean;
+    immediateCallback?: boolean;
 }
 
 export function debounce<F extends Fn>(fn: F, ms: number = 0, options: DebounceOptions = {}) {
-    let { immediate = false } = options;
+    let { immediate = false, immediateCallback = false } = options;
     let timeoutId: ReturnType<typeof setTimeout> | null = null;
 
     function debounceWrap(this: unknown, ...args: Parameters<F>) {
@@ -19,7 +20,9 @@ export function debounce<F extends Fn>(fn: F, ms: number = 0, options: DebounceO
             return fn.apply(null, args);
         } else {
             timeoutId = setTimeout(() => {
-                !immediate && (immediate = true);
+                if (immediateCallback) {
+                    !immediate && (immediate = true);
+                }
                 return fn.apply(this, args);
             }, ms);
         }
